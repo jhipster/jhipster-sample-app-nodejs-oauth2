c3eab077fb620fa5d72f04f200dae28225cd2eae
Angelo Manganiello <angelo.mang@libero.it>
2020-09-03 14:56:59 +0200

    Initial version of gen generated by JHipster-6.8.0 with blueprints: nodejs-1.2.0

--- server/src/security/passport.oauth2.strategy.ts
+++ server/src/security/passport.oauth2.strategy.ts
@@ -1,6 +1,7 @@
+import jwtDecode from 'jwt-decode';
 import Strategy = require('passport-oauth2');
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, UnauthorizedException, Logger, HttpService } from '@nestjs/common';
+import { Injectable, UnauthorizedException, Logger } from '@nestjs/common';
 import { config } from '../config';
 import { AuthService } from '../service/auth.service';
 import { oauth2Config } from './oauth2.config';
@@ -12,7 +13,7 @@ const clientSecret = config.get('jhipster.security.oauth2.client.registration.oi
 export class Oauth2Strategy extends PassportStrategy(Strategy) {
   logger = new Logger('oauth2');
 
-  constructor(private readonly authService: AuthService, private readonly httpService: HttpService) {
+  constructor(private readonly authService: AuthService) {
     super(
       {
         authorizationURL: oauth2Config.authorizationURL,
@@ -33,39 +34,27 @@ export class Oauth2Strategy extends PassportStrategy(Strategy) {
     );
   }
 
-  async userProfile(accessToken: any, done: any): Promise<any> {
-    // roles with id http://dev-281272.okta.com/api/v1/users/<id>/roles
-    // id in http://dev-281272.okta.com/api/v1/users/me
-    return await this.httpService
-      .get(oauth2Config.userInfoUrl, {
-        headers: {
-          // Include the token in the Authorization header
-          Authorization: 'Bearer ' + accessToken
-        }
-      })
-      .toPromise()
-      .then(res => {
-        const profile = res.data;
+  userProfile(accessToken: any, done: any): Promise<any> {
+    try {
+      const profile: any = jwtDecode(accessToken);
+      const userProfile = {
+        login: profile.preferred_username,
+        password: '***',
+        firstName: profile.given_name,
+        lastName: profile.family_name,
+        email: profile.email,
+        imageUrl: '',
+        activated: true,
+        langKey: 'en',
+        createdBy: 'system',
+        lastModifiedBy: 'system',
+        authorities: ['ROLE_ADMIN', 'ROLE_USER']
+      };
 
-        const userProfile = {
-          login: profile.preferred_username,
-          password: '***',
-          firstName: profile.given_name,
-          lastName: profile.family_name,
-          email: profile.email,
-          imageUrl: '',
-          activated: true,
-          langKey: 'en',
-          createdBy: 'system',
-          lastModifiedBy: 'system',
-          authorities: ['ROLE_ADMIN', 'ROLE_USER']
-        };
-
-        return done(null, userProfile);
-      })
-      .catch(e => {
-        this.logger.error(e);
-        return done(new UnauthorizedException({ message: 'error to retrieve user info from accessToken' }), false);
-      });
+      return done(null, userProfile);
+    } catch (e) {
+      this.logger.error(e);
+      return done(new UnauthorizedException({ message: 'error to retrieve user info from accessToken' }), false);
+    }
   }
 }
